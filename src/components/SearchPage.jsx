b40a5216b375fb78765ae5e8b959f9371d4b5deb
Aryan <aryansajan630@gmail.com>
2024-07-21 08:11:52 +0530

    updated User-List

--- src/components/SearchPage.jsx
+++ src/components/SearchPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from './Navbar'
 
-const AddPage = () => {
+const SearchPage = () => {
     return (
         <div>
             <Navbar/>
@@ -9,28 +9,12 @@ const AddPage = () => {
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                         <div className="row g-3">
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                                 <label htmlFor="" className="form-label">Name</label>
                                 <input type="text" className="form-control" />
                             </div>
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                            <label htmlFor="" className="form-label">Username</label>
-                            <input type="text" className="form-control" />
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                            <label htmlFor="" className="form-label">Email</label>
-                            <input type="text" className="form-control" />
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                            <label htmlFor="" className="form-label">Phone No.</label>
-                            <input type="text" className="form-control" />
-                            </div>
                             <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                            <label htmlFor="" className="form-label">Website</label>
-                            <input type="text" className="form-control" />
-                            </div>
-                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                                <button className="btn btn-info">Submit</button>
+                                <button className="btn btn-info">Search</button>
                             </div>
                         </div>
                     </div>
@@ -40,4 +24,4 @@ const AddPage = () => {
     )
 }
 
-export default AddPage
+export default SearchPage
